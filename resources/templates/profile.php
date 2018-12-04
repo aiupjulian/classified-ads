6d046ba87122dbe8df75c50bc64fc1a38482f5f8
Julian Aiup <aiupjulian@gmail.com>
2018-12-03 21:54:01 -0300

    Add user data and ads to profile

--- resources/templates/profile.php
+++ resources/templates/profile.php
@@ -15,5 +15,42 @@ $query = "SELECT * FROM user WHERE user.id = '$user_id'";
 $userResult = mysqli_query($link, $query);
 $user = mysqli_fetch_array($userResult, MYSQLI_ASSOC);
 
-echo $user['name'];
+$query = "SELECT * FROM ad WHERE ad.user_id = '$user_id'";
+$adsResult = mysqli_query($link, $query);
 ?>
+
+<h2 class="form-title">User data:</h2>
+<div class="form">
+  <div>Name: <?php echo $user['name']; ?></div>
+  <div>Phone: <?php echo $user['phone']; ?></div>
+  <div>Email: <?php echo $user['email']; ?></div>
+</div>
+
+<h2 class="form-title">User ads:</h2>
+<ul class="ads-container">
+  <div class="ads-list">
+  <?php
+  while ($ad = mysqli_fetch_array($adsResult, MYSQLI_ASSOC)) {
+  ?>
+    <li>
+      <a href="<?php echo "/ad.php?id=" . $ad['id'] ?>">
+        <img src="<?php echo "images/uploaded/" . $ad['image']; ?>" />
+        <div class="ad-container">
+          <div class="ad-name"><?php echo $ad['name']; ?></div>
+          <div class="ad-price">$<?php echo $ad['price']; ?></div>
+          <div class="ad-date"><?php echo $ad['date']; ?></div>
+          <div><?php echo $ad['sold'] ? 'Sold' : 'Not sold'; ?></div>
+        </div>
+      </a>
+      <div class="actions">
+        <a class="button">Edit</a>
+        <a class="button">Delete</a>
+        <a class="button">Mark as sold</a>
+      </div>
+    </li>
+  <?php
+  }
+  close($link);
+  ?>
+  </div>
+</ul>
